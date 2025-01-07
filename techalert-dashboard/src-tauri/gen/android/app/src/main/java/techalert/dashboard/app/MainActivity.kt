0df4878e7052ef2b5cc9628c9e88a9c70aeaae36
mrndstvndv <stvndvmrnd@gmail.com>
2025-01-07 10:42:03 +0800

    feat: request notification permission on start

--- techalert-dashboard/src-tauri/gen/android/app/src/main/java/techalert/dashboard/app/MainActivity.kt
+++ techalert-dashboard/src-tauri/gen/android/app/src/main/java/techalert/dashboard/app/MainActivity.kt
@@ -1,3 +1,41 @@
 package techalert.dashboard.app
 
-class MainActivity : TauriActivity()
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+class MainActivity : TauriActivity() {
+  val RC_NOTIFICATION = 99
+
+  private fun getNotificationPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      if (
+        ContextCompat.checkSelfPermission(
+          this,
+          Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), RC_NOTIFICATION)
+      }
+    }
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    
+    if (requestCode == RC_NOTIFICATION) {
+      if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+        Toast.makeText(this, "Please allow notifications so we can send you alerts", Toast.LENGTH_SHORT).show()
+      }
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    
+    getNotificationPermission()
+  }
+}
